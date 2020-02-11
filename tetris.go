be43ddedc2095504d1700aeedc357ce6ff6e53fa
gkuehn001 <gkuehn@gmail.com>
2020-02-11 19:43:38 +0100

    added basic game loop

--- tetris.go
+++ tetris.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"time"
+
 	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
+
+var dropTime = 1000 // ms
+var info = "Event Test"
 
 func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 	for _, c := range msg {
@@ -21,10 +26,30 @@ func draw() {
 	midy := h / 2
 	midx := w / 2
 
-	tbprint(midx-9, midy-1, coldef, coldef, "Let's code Tetris!")
+	tbprint(midx-9, midy-1, coldef, coldef, info)
 	tbprint(midx-9, midy+1, coldef, coldef, "Press ESC to quit")
 
 	termbox.Flush()
+}
+
+func onKeyDown() {
+	info = "Down"
+}
+
+func onKeyLeft() {
+	info = "Left"
+}
+
+func onKeyRight() {
+	info = "Right"
+}
+
+func onKeyUp() {
+	info = "Up"
+}
+
+func onDrop() {
+	info = "Drop"
 }
 
 func main() {
@@ -37,19 +62,41 @@ func main() {
 
 	termbox.SetInputMode(termbox.InputEsc)
 
+	eventQueue := make(chan termbox.Event)
+	go func() {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
+
+	dropTick := time.NewTicker(time.Duration(dropTime) * time.Millisecond)
+
 	draw()
 
 mainloop:
 	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			switch ev.Key {
-			case termbox.KeyEsc:
-				break mainloop
-			default:
+		select {
+		case ev := <-eventQueue:
+			switch ev.Type {
+			case termbox.EventKey:
+				switch ev.Key {
+				case termbox.KeyEsc:
+					break mainloop
+				case termbox.KeyArrowDown:
+					onKeyDown()
+				case termbox.KeyArrowLeft:
+					onKeyLeft()
+				case termbox.KeyArrowRight:
+					onKeyRight()
+				case termbox.KeyArrowUp:
+					onKeyUp()
+				default:
+				}
+			case termbox.EventError:
+				panic(ev.Err)
 			}
-		case termbox.EventError:
-			panic(ev.Err)
+		case <-dropTick.C:
+			onDrop()
 		}
 		draw()
 	}
